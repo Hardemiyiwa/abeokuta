f6eccd39297b01009914ab9aa4848ab8c2d3d66c
Ridwan <princeghardemuyiwa@gmail.com>
2025-09-02 12:12:58 +0100

    few tasks edited

--- python/week_1/using_escape_sequence.py
+++ python/week_1/using_escape_sequence.py
@@ -15,6 +15,26 @@ print("Aisha\t25\tLagos")
 print("She said, \"Hello there!\"")
 print('It\'s a sunny day.')
 
+# Backslash
+print("File path: C:\\Users\\Aisha\\Documents")
+
+# Carriage return (\r)
+print("123456\rABC") # Output: ABC456 (replaces 123)
+
+# Backspace (\b)
+print("Helloo\b") # Output: Hello
+
+# Bell/alert (may or may not deep depending on environment)
+# print("\a") # Triggers a bell sound (if supported)
+
+print("Hello\fworld")   # Form feed
+print("Hello\vworld")   # Vertical tab
+
+# Using sep and end in print
+print("Python", "is", "fun", sep=" - ")
+print("This is the first line.", end=" ")
+print("This is the second line.")
+
 # print("\t\t\t\t\t\t\t\t+\n\n\t\t\t\t\t\t\t+\t\t+\n\n\t\t\t\t\t\t+")
 print("        +\n       +++\n      +++++\n     +++++++\n    +++++++++\n   +++++++++++")
 print("        v\t\n       vvv\t\n      vvvvv\t\n     vvvvvvv\t\n    vvvvvvvvv\t\n   vvvvvvvvvvv\t\n  vvvvvvvvvvvvv\t\n vvvvvvvvvvvvvvv\t\nvvvvvvvvvvvvvvvvv\t\n")
